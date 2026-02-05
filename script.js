f02351962b0ae57dcdab863462facd56820668b6
Faiz-7716 <pmdfaiz08@gmail.com>
2026-02-05 20:09:10 +0530

    first commit - unsplash api project

---
+++ script.js
@@ -0,0 +1,67 @@
+const accessKey = "ys9HIn8ZSRzMtkkdTyHOaI6yf1c_NvNCqtwE9d3glLY";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+
+
+async function searchImages() {
+    keyword = searchBox.value;
+     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+     const response = await fetch(url);
+     const data = await response.json();
+
+     if (page === 1) {
+        searchResult.innerHTML = "";
+     }
+     console.log(data);
+
+     const results = data.results;
+
+     results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.style.cursor = "pointer";
+        image.addEventListener("click", () => {
+            openModal(result.urls.regular);
+        });
+        searchResult.appendChild(image);
+     });
+     showMoreBtn.style.display = "block";
+
+}
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+showMoreBtn.addEventListener("click", () => {
+    page++;
+    searchImages();
+});
+
+const modal = document.getElementById("image-modal");
+const modalImage = document.getElementById("modal-image");
+const closeBtn = document.querySelector(".close");
+
+function openModal(imageSrc) {
+    modal.style.display = "flex";
+    modalImage.src = imageSrc;
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+closeBtn.addEventListener("click", closeModal);
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
